e80c1bd3b1cfcb54f214f4f07de90ce8f2a8b1fd
Kirill Korolev <voxweblab@gmail.com>
2022-09-16 22:23:02 +0300

    Create little-lemon-receipt-maker.js

---
+++ c2w5/little-lemon-receipt-maker.js
@@ -0,0 +1,60 @@
+// Given variables
+const dishData = [
+    {
+        name: "Italian pasta",
+        price: 9.55
+    },
+    {
+        name: "Rice with veggies",
+        price: 8.65
+    },
+    {
+        name: "Chicken with potatoes",
+        price: 15.55
+    },
+    {
+        name: "Vegetarian Pizza",
+        price: 6.45
+    },
+]
+const tax = 1.20;
+
+// Implement getPrices()
+function getPrices(taxBoolean) {
+    for (dish of dishData) {
+        let finalPrice;
+        if (taxBoolean == true) {
+            finalPrice = dish.price * tax;
+        }
+        else if (taxBoolean == false) {
+            finalPrice = dish.price;
+        }
+        else {
+            console.log("You need to pass a boolean to the getrPrices call!");
+            return
+        }
+        console.log("Dish: " + dish.name, "Price: $" + finalPrice);
+    }
+}
+
+// Implement getDiscount()
+function getDiscount(taxBoolean, guests) {
+    getPrices(taxBoolean);
+    let condition = typeof(guests) == 'number' && guests > 0 && guests < 30;
+    if (condition) {
+        let discount = 0;
+        if (guests < 5) {
+            discount = 5;
+        }
+        else if (guests >= 5) {
+            discount = 10;
+        }
+        console.log('Discount is: $' + discount);
+    }
+    else {
+        console.log('The second argument must be a number between 0 and 30');
+    }
+}
+
+// Call getDiscount()
+getDiscount(true, 10);
